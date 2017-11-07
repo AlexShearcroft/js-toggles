dc9020f2510805ec6885d200f8dc69f502a52be5
alexshearcroft <alexshearcroft@gmail.com>
2017-11-07 21:58:44 +0000

    start of prototype function

---
+++ toggles.js
@@ -0,0 +1,30 @@
+function Toggles () {
+
+	// The area to toggle up and down
+	this.toggleArea = 'data-toggle';
+
+}
+
+/**
+ * Toggles a div area up and down
+ * @param {object} element
+ */
+Toggles.prototype.toggle = function (element) {
+
+    var area = element.attr(this.toggleArea);
+
+    $('#js-' + area).toggle();
+}
+
+/**
+ * Slidetoggles a div area up and down
+ * @param {object} element
+ */
+Toggles.prototype.slideToggle = function (element) {
+
+    var area = element.attr(this.toggleArea);
+
+    $('#js-' + area).slideToggle();
+}
+
+Tog = new Toggles();
